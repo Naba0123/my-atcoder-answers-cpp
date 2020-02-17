c8733210d397f383328bc05732b374b77897f138
Naba <11944066+Naba0123@users.noreply.github.com>
2020-02-17 23:46:03 +0900

    abc155/a

---
+++ abc155/a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if (a == b && a == c) {
+        cout << "No" << endl;
+    } else if (a == b || a == c || b == c) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+
+    return 0;
+}
