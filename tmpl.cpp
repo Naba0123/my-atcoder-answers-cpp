3b0638dce24953a52354b134d91549c053d18ca5
Naba <11944066+Naba0123@users.noreply.github.com>
2020-02-25 14:51:07 +0900

    change format to Google

--- tmpl.cpp
+++ tmpl.cpp
@@ -3,8 +3,7 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define rep_s(i, s, n) for (int i = (int)(s); i < (int)(n); i++)
 
-int main()
-{
-    int N;
-    cin >> N;
+int main() {
+  int N;
+  cin >> N;
 }
